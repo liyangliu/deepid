a42938150784abe7081814f55b06d62085d9dc89
Liyang Liu <liuliyang@1231@gmail.com>
2016-09-24 12:45:19 +0800

    add get_shape.sh

---
+++ examples/deepid/tools/get_loss_acc.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+set -e
+
+LOG_FILE=models/deepid/result/solver.log
+TRAIN_LOSS=models/deepid/result/train_loss
+TEST_LOSS=models/deepid/result/test_loss
+TEST_ACC=models/deepid/result/test_acc
+
+GLOG_logtostderr=1 cat \
+    $LOG_FILE \
+    | grep "Train net output" \
+    | awk '{print $11}' \
+    > $TRAIN_LOSS
+
+GLOG_logtostderr=1 cat \
+    $LOG_FILE \
+    | grep "Test net output #1" \
+    | awk '{print $11}' \
+    > $TEST_LOSS
+
+GLOG_logtostderr=1 cat \
+    $LOG_FILE \
+    | grep "Test net output #0" \
+    | awk '{print $11}' \
+    > $TEST_ACC
+
+echo "Done."
